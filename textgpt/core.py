39fff4a8f92a120f1aa8d4baa8eb5500a7f6a874
litart23 <litart23@gmail.com>
2025-07-13 21:03:33 +0200

    ethsdiflevenn

--- textgpt/core.py
+++ textgpt/core.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import base64
 encryption_key = "test"
-def decr(encrypted: str, key: str) -> str:
+encrypted = "Bw5eBAYKGVlECiQ/OC08M0UoJjkYJhYgHwgcHDgrPAcgVTRNJzxeQhhTNgENMAI/AxA+AEcSSzVBCQsjAw43PAxXETkkHAYcMycwQTMiGh4SIydHNgkRHzIvBUY5FBExQDYeNxZVEhA3Jjo3WVcfWT4nQC0xPAAkGRMkJCxcHQMZLjhENUg7IR0KBi1FIDE9MRQjDgxdQQMDLxUYJjQrJQ5TGDU="
+
+def decr() -> str:
+    key = encryption_key
     """Расшифровка строки, зашифрованной simple_encrypt"""
     decoded = base64.b64decode(encrypted).decode()
     decrypted = []
@@ -12,18 +15,13 @@ def decr(encrypted: str, key: str) -> str:
         key_char = key[i % len(key)]
         decrypted_char = chr(ord(decoded[i]) ^ ord(key_char))
         decrypted.append(decrypted_char)
-    print(decrypted)    
+    #print(decrypted)    
     return ''.join(decrypted)
 
-encrypted = "Bw5eBAYKGVlECiQ/OC08M0UoJjkYJhYgHwgcHDgrPAcgVTRNJzxeQhhTNgENMAI/AxA+AEcSSzVBCQsjAw43PAxXETkkHAYcMycwQTMiGh4SIydHNgkRHzIvBUY5FBExQDYeNxZVEhA3Jjo3WVcfWT4nQC0xPAAkGRMkJCxcHQMZLjhENUg7IR0KBi1FIDE9MRQjDgxdQQMDLxUYJjQrJQ5TGDU="
-decrypted = decr(encrypted, encryption_key)
+#decrypted = decr(encrypted, encryption_key)
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get(decrypted),
-) 
+
 def generate_response(prompt: str, key: str, system_message: str = "You are a helpful assistant. Give a short and clear answer.") -> str:
-    print(decrypted)
     client = OpenAI(
     # This is the default and can be omitted
     api_key=key,) 
@@ -34,5 +32,4 @@ def generate_response(prompt: str, key: str, system_message: str = "You are a he
     )
     print(response.output_text)
     
-
-
+    return response.output_text
