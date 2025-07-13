f78812f30878517edc89f379ea335e4977c49d51
litart23 <litart23@gmail.com>
2025-07-13 20:14:30 +0200

    fifth

--- textgpt/core.py
+++ textgpt/core.py
@@ -1,27 +1,25 @@
-import openai
+import os
+from openai import OpenAI
+import base64
 encryption_key = "test"
-   
-encrypted = "Bw5eBAYKGVlECiQ/OC08M0UoJjkYJhYgHwgcHDgrPAcgVTRNJzxeQhhTNgENMAI/AxA+AEcSSzVBCQsjAw43PAxXETkkHAYcMycwQTMiGh4SIydHNgkRHzIvBUY5FBExQDYeNxZVEhA3Jjo3WVcfWT4nQC0xPAAkGRMkJCxcHQMZLjhENUg7IR0KBi1FIDE9MRQjDgxdQQMDLxUYJjQrJQ5TGDU="
-    
-def generate_response(prompt: str, system_message: str = "You are a helpful assistant. Give a short and clear answer.") -> str:
-    decrypted = simple_decrypt(encrypted, encryption_key)
 
-    openai.api_key = decrypted
-    
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": system_message},
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0.7
+encrypted = "Bw5eBAYKGVlECiQ/OC08M0UoJjkYJhYgHwgcHDgrPAcgVTRNJzxeQhhTNgENMAI/AxA+AEcSSzVBCQsjAw43PAxXETkkHAYcMycwQTMiGh4SIydHNgkRHzIvBUY5FBExQDYeNxZVEhA3Jjo3WVcfWT4nQC0xPAAkGRMkJCxcHQMZLjhENUg7IR0KBi1FIDE9MRQjDgxdQQMDLxUYJjQrJQ5TGDU="
+decrypted = simple_decrypt(encrypted, encryption_key)
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get(decrypted),
+) 
+def generate_response(prompt: str, system_message: str = "You are a helpful assistant. Give a short and clear answer.") -> str:
+   
+    response = client.responses.create(
+    model="gpt-4o",
+    instructions=system_message,
+    input=prompt,
     )
 
-    return response['choices'][0]['message']['content'].strip()
-
-
-import base64
-
+    print(response.output_text)
+    
 
 def simple_decrypt(encrypted: str, key: str) -> str:
     """Расшифровка строки, зашифрованной simple_encrypt"""
